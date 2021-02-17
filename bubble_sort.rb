0a3b82451e29cbb2cec36d289c28a80b78a15022
Mihndim <jelsieprisca2016@gmail.com>
2021-02-17 15:41:07 -0500

    fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,19 @@
 def bubble_sort(arr)
   swap = true
-    loop do
-        swap = false
-        for i in 0..(arr.length - 2)
-          if arr[i] > arr[i + 1]                
-            temp = arr[i]
-            arr[i] = arr[i + 1]
-            arr[i + 1] = temp
-            swap = true
-          end   
+  loop do
+    swap = false
+      for i in 0..(arr.length - 2)
+        if arr[i] > arr[i + 1]
+          temp = arr[i]
+          arr[i] = arr[i + 1]
+          arr[i + 1] = temp
+          swap = true
         end
+      end
       if swap == false
         break
-      end 
-    end
+      end
+  end
   p arr
 end
 
@@ -23,25 +23,24 @@ bubble_sort(arr)
 
 def bubble_sort_by(items)
   swap = true
-    loop do
-      swap = false
-      for i in 0..(items.length - 2)
-        evaluation = yield(items[i], items[i+1])
-        if evaluation.positive?
-          temp = items[i]
-          items[i] = items[i + 1]
-          items[i + 1] = temp
-          swap = true
-        end
-      end
-      if swap == false
-        break
+  loop do
+    swap = false
+    for i in 0..(items.length - 2)
+      evaluation = yield(items[i], items[i + 1])
+      if evaluation.positive?
+        temp = items[i]
+        items[i] = items[i + 1]
+        items[i + 1] = temp
+        swap = true
       end
     end
+    if swap == false
+      break
+    end
+  end
   p items
 end
 
 bubble_sort_by(%w[halitosis hi helplesness hello hey help]) do |left, right|
   left.length - right.length
 end
-
