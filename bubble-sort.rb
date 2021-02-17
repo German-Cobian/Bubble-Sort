64fb076fa1ce9de72bad9e11e1595dc8921207ec
Mihndim <jelsieprisca2016@gmail.com>
2021-02-17 11:36:18 -0500

    add swapping loop

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,7 +1,11 @@
 def bubble_sort(arr)
   (arr.length).times do
     for i in 0..(arr.length - 2)
-        
+      if arr[i] > arr[i + 1]
+          temp = arr[i]
+          arr[i] = arr[i + 1]
+          arr[i + 1] = temp
+        end   
     end
 end
 return arr
