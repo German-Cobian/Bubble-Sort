4e1bb7dd1b64b03d92fb3b11d89fc30c74342a28
Mihndim <jelsieprisca2016@gmail.com>
2021-02-17 10:54:01 -0500

    setup linters and define bubble sort method

---
+++ bubble-sort.rb
@@ -0,0 +1,19 @@
+
+
+
+def bubble_sort(arr)
+    (arr.length).times do
+        for i in 0..(arr.length - 2)
+            if arr[i] > arr[i + 1]
+                temp = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = temp
+            end
+        end
+    end
+    return arr
+end 
+
+arr = [4, 3, 78, 2, 0, 2]
+
+p bubble_sort(arr)
