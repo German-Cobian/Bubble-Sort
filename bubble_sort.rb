e93c36ec0a30f370447addfbdcfa3445b59c9a1e
Mihndim <jelsieprisca2016@gmail.com>
2021-02-17 19:27:48 -0500

    Refactor code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,34 @@
 def bubble_sort(arr)
   swap = true
-  loop do
+
+  while swap
     swap = false
-    for i in 0..(arr.length - 2)
+
+    0.upto(arr.length - 2) do |i|
       if arr[i] > arr[i + 1]
-        temp = arr[i]
-        arr[i] = arr[i + 1]
-        arr[i + 1] = temp
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swap = true
       end
     end
-    if swap == false
-      break
+  end
+
+  p arr
+end
+
+def bubble_sort_by(arr)
+  swap = true
+
+  while swap
+    swap = false
+
+    0.upto(arr.length - 2) do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swap = true
+      end
     end
   end
+
   p arr
 end
 
@@ -21,26 +36,6 @@ arr = [4, 3, 78, 2, 0, 2]
 
 bubble_sort(arr)
 
-def bubble_sort_by(items)
-  swap = true
-  loop do
-    swap = false
-    for i in 0..(items.length - 2)
-      evaluation = yield(items[i], items[i + 1])
-      if evaluation.positive?
-        temp = items[i]
-        items[i] = items[i + 1]
-        items[i + 1] = temp
-        swap = true
-      end
-    end
-    if swap == false
-      break
-    end
-  end
-  p items
-end
-
-bubble_sort_by(%w[halitosis hi helplesness hello hey help]) do |left, right|
-  left.length - right.length
+ bubble_sort_by(%w[halitosis hi helplesness hello hey help]) do |left, right|
+   left.length - right.length
 end
